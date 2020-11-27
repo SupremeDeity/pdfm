fbe9b4d5f6fb6ac5cd92580523ad634891a86c16
SupremeDeity <sheikhmohsin706@gmail.com>
2020-11-27 18:51:04 +0500

    v0.2.1: Added list options and guards

--- pdfm.sh
+++ pdfm.sh
@@ -7,15 +7,15 @@ print_help() {
 	echo "There are following options: "
 	echo -e "init\t\t Initializes a git repo and creates a track file."
 	echo -e "add\t\t Tracks files using trackfile argument."
+	echo -e "list\t\t Prints all files being tracked by git."
 	echo -e "-h | --help \t Prints this help menu."
 	echo -e "-v | --version \t Prints PDFM version."
-
 	exit
 }
 
 # Prints PDFM version to screen
 print_version() {
-	echo "PDFM v0.1"
+	echo "PDFM v0.2.1"
 	exit
 }
 
@@ -29,20 +29,36 @@ init() {
 
 	exit
 }
+
+# List Trackfile
+tfList() {
+   	echo -e "$(git ls-tree -r master --name-only)"
+		exit
+}
+
 # Track Modified, deleted, new files
 add() {
 	git_dir=$(git rev-parse --show-toplevel)
 	file="$git_dir/track.pdfm"
-	local crdir="$PWD"
-	cd "$git_dir"
-	# Due to -n incomplete line may be read. This should be fine in most cases.
-	# https://unix.stackexchange.com/questions/482517/why-does-this-while-loop-not-recognize-the-last-line
-	while IFS= read -r line || [ -n "$line" ];
-	do
-		git add "$line"
-	done < "$file"
 	
-	cd $crdir
+	if [ -f "$file" ]; then
+		local crdir="$PWD"
+		cd "$git_dir"
+		# Due to -n incomplete line may be read. This should be fine in most cases.
+		# https://unix.stackexchange.com/questions/482517/why-does-this-while-loop-not-recognize-the-last-line
+		while IFS= read -r line || [ -n "$line" ];
+		do
+			if [ "$line" = "" ]; then
+				echo "Warning: Empty line detected. Please remove any empty lines or populate track.pdfm if you have not done it already."
+			else
+				git add "$line"
+			fi
+		done < "$file"
+	
+		cd $crdir
+	else
+		echo "The current repository is not managed by PDFM."
+	fi
 
 	exit
 }
@@ -53,15 +69,17 @@ if [ ! $# -gt 0 ]; then
 else
 	while [ "$1" != "" ]; do
 		case $1 in
-			init )			init
+			init 						)		init
 							;;
-			add )			add "$@"
+			add 						)		add "$@"
 							;;
-			-h | --help ) 	print_help
+			list            )   tfList
 							;;
-			-v | --version) print_version
+			-h | --help 		) 	print_help
 							;;
-			* ) 			print_help
+			-v | --version	) 	print_version
+							;;
+			* ) 								print_help
 							;;
 			
 		esac
