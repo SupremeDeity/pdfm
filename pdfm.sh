4f122c2f788808536eeace4d2f00ad683f039561
SupremeDeity <sheikhmohsin706@gmail.com>
2021-02-02 16:21:07 +0500

    PDFM Release V1.0.0
    > --list now prints the trackfile instead.
    
    > A simple message is printed instead on wrong command.
    
    > The help example command has been fixed.

--- pdfm.sh
+++ pdfm.sh
@@ -2,20 +2,25 @@
 
 # Prints help to screen
 print_help() {
-	echo -e "usage: pdfm <subcommand> <args> [options]\n"
+	echo -e "usage: pdfm <args>\n"
 
-	echo "There are following options: "
+	echo "The following args can be passed to pdfm: "
 	echo -e "init\t\t Initializes a git repo and creates a track file."
-	echo -e "add\t\t Tracks files using trackfile argument."
-	echo -e "list\t\t Prints all files being tracked by git."
+	echo -e "add\t\t Tracks files using trackfile."
+	echo -e "-l | --list \t Prints track file with location."
 	echo -e "-h | --help \t Prints this help menu."
 	echo -e "-v | --version \t Prints PDFM version."
 	exit
 }
 
+print_unknown() {
+	echo -e "Unknown or mismatched arguments. Use --help for help."
+	exit
+}
+
 # Prints PDFM version to screen
 print_version() {
-	echo "PDFM v0.3.0"
+	echo "PDFM v1.0.0"
 	exit
 }
 
@@ -30,10 +35,31 @@ init() {
 	exit
 }
 
-# List Trackfile
-tfList() {
-   	echo -e "$(git ls-tree -r master --name-only)"
-		exit
+printTrack() {
+	git_dir=$(git rev-parse --show-toplevel)
+	file="$git_dir/track.pdfm"
+
+	if [ -f "$file" ]; then
+		local crdir="$PWD"
+		cd "$git_dir"
+
+		echo -e "Track File Location: $file"
+		# Due to -n incomplete line may be read. This should be fine in most cases.
+		# https://unix.stackexchange.com/questions/482517/why-does-this-while-loop-not-recognize-the-last-line
+		while IFS= read -r line || [ -n "$line" ];
+		do
+			if [ -n "$line" ]; then
+				[[ $line =~ ^#.* ]] && continue
+				echo $line
+			fi
+		done < "$file"
+
+		cd $crdir
+	else
+		echo "The current repository is not managed by PDFM."
+	fi
+
+	exit
 }
 
 # Track Modified, deleted, new files
@@ -68,17 +94,17 @@ if [ ! $# -gt 0 ]; then
 else
 	while [ "$1" != "" ]; do
 		case $1 in
-			init 						)		init
+			init 						)	init
 							;;
-			add 						)		add "$@"
+			add 						)	add
 							;;
-			list            )   tfList
+			-l | --list            		)   tfList
 							;;
-			-h | --help 		) 	print_help
+			-h | --help 				) 	print_help
 							;;
-			-v | --version	) 	print_version
+			-v | --version				) 	print_version
 							;;
-			* ) 								print_help
+			* 							) 	print_unknown
 							;;
 			
 		esac
